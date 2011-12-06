fe303df5330ef7102afe85aa99094a9ce65fe19c
Rick Wang <rickwangtw@hotmail.com>
2011-12-06 17:13:38 +0800

    Add Android.mk to build file command

---
+++ src/Android.mk
@@ -0,0 +1,46 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES := \
+	apprentice.c	\
+	apptype.c	\
+	ascmagic.c	\
+	asprintf.c	\
+	cdf.c	\
+	cdf_time.c	\
+	compress.c	\
+	encoding.c	\
+	file.c	\
+	fsmagic.c	\
+	funcs.c	\
+	getline.c	\
+	getopt_long.c	\
+	is_tar.c	\
+	magic.c	\
+	print.c	\
+	readcdf.c	\
+	readelf.c	\
+	softmagic.c	\
+	strlcat.c	\
+	strlcpy.c	\
+	vasprintf.c	\
+
+LOCAL_CFLAGS :=	\
+	-DHAVE_CONFIG_H	\
+	-DANDROID
+
+LOCAL_MODULE:= file
+
+LOCAL_MODULE_TAGS:= optional
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)	\
+	$(LOCAL_PATH)/../	\
+	$(TOP)/external/zlib/
+
+LOCAL_SHARED_LIBRARIES	:=	\
+	libz
+
+
+include $(BUILD_EXECUTABLE)
